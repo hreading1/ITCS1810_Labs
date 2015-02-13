3e533cbd39b2ba012160e972a19f91115673d3ca
hreading1 <hreading1@mail.lakelandcc.edu>
2015-02-12 19:17:59 -0500

    lab2finished

--- lab2vb/lab2vb/Module1.vb
+++ lab2vb/lab2vb/Module1.vb
@@ -18,12 +18,37 @@
         Pet1.Owner = Niece
 
         Dim Pet2 As Pet = New Pet
-        Pet1.Name = "Max"
-        Pet1.Age = 16
-        Pet1.Species = "Cat"
-        Pet1.Owner = Nephew
+        Pet2.Name = "Max"
+        Pet2.Age = 16
+        Pet2.Species = "Cat"
+        Pet2.Owner = Nephew
 
+        Dim Pet3 As Pet = New Pet
+        Pet3.Name = "Buddy"
+        Pet3.Age = 3
+        Pet3.Species = "Cat"
+        Pet3.Owner = Niece
 
+        Console.WriteLine(Pet1.Name)
+        Console.WriteLine(Pet1.Age)
+        Console.WriteLine(Pet1.Species)
+        Console.WriteLine(Pet1.Owner.FullName)
+
+        Console.WriteLine()
+
+        Console.WriteLine(Pet2.Name)
+        Console.WriteLine(Pet2.Age)
+        Console.WriteLine(Pet2.Species)
+        Console.WriteLine(Pet2.Owner.FullName)
+
+        Console.WriteLine()
+
+        Console.WriteLine(Pet3.Name)
+        Console.WriteLine(Pet3.Age)
+        Console.WriteLine(Pet3.Species)
+        Console.WriteLine(Pet3.Owner.FullName)
+
+        Console.ReadKey()
 
     End Sub
 
--- lab2vb/lab2vb/Person.vb
+++ lab2vb/lab2vb/Person.vb
@@ -3,4 +3,7 @@
     Public FirstName As String
     Public LastName As String
 
+    Public Function FullName() As String
+        Return FirstName & " " & LastName
+    End Function
 End Class
